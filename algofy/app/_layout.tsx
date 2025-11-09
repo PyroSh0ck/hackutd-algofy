22a8f04a622c52e5a06253b78e9e6cfe61539aac
PyroSh0ck <parthmodi.amodi@gmail.com>
2025-11-09 07:57:38 -0600

    added features for datasharing with modals

--- algofy/app/_layout.tsx
+++ algofy/app/_layout.tsx
@@ -34,6 +34,7 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="(signup)" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
